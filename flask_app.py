484e42ea00daaf33975f2d3d709a8c49a5faf605
MrHed <rasmus.h93@gmail.com>
2019-10-21 17:05:41 +0200

    Count workers

--- flask_app.py
+++ flask_app.py
@@ -1,9 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route('/QTL/test')
+def test():
+    return 'Hello world!'
 
-@app.route('/QTL/hosts')
+@app.route('/QTL/count_workers')
 def count_hosts():
     num = 0
     files = open('/etc/ansible/hosts', 'r')
@@ -16,12 +19,15 @@ def count_hosts():
     return num
 
 
+
 @app.route('/QTL/run', methods=['GET'])
 def run_task():
     """
-    Run the task
+    Run the heat template
     """
-    return "Hello World \n"
+    setup = render_template('setup.yml', title='Setup')
+    
+    return setup
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
